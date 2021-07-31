8f105cf20dd749247cb9d446fee70b8d458d4c13
angelxehg <sudo@angelxehg.com>
2021-07-31 10:47:49 -0500

    Fetch from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ class Blog extends Component {
     super(props);
     this.state = {
       articulos: [
-        { title: 'Articulo 1' },
-        { title: 'Articulo 2' },
-        { title: 'Articulo 3' }
+        {
+          userId: 1,
+          id: 1,
+          title:
+            'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+          body:
+            'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+        }
       ]
     };
+  }
+  async componentDidMount() {
+    const respuesta = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const datos = await respuesta.json();
+    this.setState({ articulos: datos });
   }
   render() {
     const { articulos } = this.state;
@@ -20,6 +30,7 @@ class Blog extends Component {
         {articulos.map(articulo => (
           <article>
             <h2>{articulo.title}</h2>
+            <p>{articulo.body}</p>
           </article>
         ))}
       </section>
